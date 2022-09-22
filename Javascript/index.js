abcb12f88599594bf933c65e830a72300ffcfb09
VictorVelasco <dani_gt_10@hotmal.com>
2022-09-22 00:21:51 -0500

    Modificaciones en nombres de imagen

--- Javascript/index.js
+++ Javascript/index.js
@@ -159,10 +159,10 @@ function imprimirProductos(){
         column.id=`columna-${producto.id}`;
         column.innerHTML=`
     <div class="card">
-    <img class="card-img-top ml-4" style="width:300px" src="./Imagenes/${producto.categoria.replace(`/\s/g`,`%20`)}.svg" alt="Card café">
+    <img class="card-img-top ml-4" style="width:300px" src="./Imagenes/${producto.categoria.replace(`/\s/g`,`-`)}.svg" alt="Card café">
         <div class="card-body">
             <h4 class="card-title">
-            ID:<strong> ${producto.id}: ${producto.nombre}</strong>
+            ID<strong> ${producto.id}: ${producto.nombre}</strong>
             </h4>
             <h4 class="card-title">
             <strong>${producto.categoria}</strong>
